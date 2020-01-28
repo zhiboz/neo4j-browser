denidaja/neo4j

Edit and create properties on items. WIP: Inspector is not updating.

src/browser/components/Form.jsx
@@ -32,7 +32,19 @@ const StyledSettingTextInput = styled.input`
   width: 192px;
 `
 
-const StyledCheckbox = styled.input`margin-right: 10px;`
+const StyledTextarea = styled.textarea`
+  color: #555;
+  font-size: 14px;
+  padding: 6px 12px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 192px;
+`
+
+const StyledCheckbox = styled.input`
+  margin-right: 10px;
+`
 const StyledLabel = styled.label`
   margin-left: 10px;
   display: inline-block;
@@ -40,11 +52,18 @@ const StyledLabel = styled.label`
     text-transform: uppercase;
   }
 `
-const StyledRadioEntry = styled.div`margin: 10px 0;`
+const StyledRadioEntry = styled.div`
+  margin: 10px 0;
+`
 
 export const TextInput = props => {
   const { children, ...rest } = props
   return <StyledSettingTextInput {...rest}>{children}</StyledSettingTextInput>
+}
+
+export const Textarea = props => {
+  const { children, ...rest } = props
+  return <StyledTextarea {...rest}>{children}</StyledTextarea>
 }
 
 export const CheckboxSelector = props => {

zhiboz/neo4j-browser

src/browser/components/Form.jsx
@@ -81,14 +81,24 @@ const StyledSettingTextInput = styled(StyledInput)`
   width: 192px;
 `
 
-const StyledCheckbox = styled.input`
-  margin-right: 10px;
-`
 const StyledRadio = styled.input`
   margin-right: 10px;
 `
-export const StyledLabel = styled.label`
-  /* margin-left: 10px; */
+const StyledTextarea = styled.textarea`
+  color: #555;
+  font-size: 14px;
+  padding: 6px 12px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 192px;
+`
+
+const StyledCheckbox = styled.input`
+  margin-right: 10px;
+`
+const StyledLabel = styled.label`
+  margin-left: 10px;
   display: inline-block;
   font-weight: 600;
   vertical-align: middle;
@@ -108,6 +118,11 @@ const StyledRadioEntry = styled.div`
 export const TextInput = props => {
   const { children, ...rest } = props
   return <StyledSettingTextInput {...rest}>{children}</StyledSettingTextInput>
+}
+
+export const Textarea = props => {
+  const { children, ...rest } = props
+  return <StyledTextarea {...rest}>{children}</StyledTextarea>
 }
 
 export const CheckboxSelector = props => {